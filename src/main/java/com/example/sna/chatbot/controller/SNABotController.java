697b30f98140ae17b5c52964314e201b0b6c6ebe
swathi.nadipelli <swathi.nadipelli@GGKU5DELL1100.ggktech.local>
2020-06-21 13:26:31 +0530

    Added cors filter

--- src/main/java/com/example/sna/chatbot/controller/SNABotController.java
+++ src/main/java/com/example/sna/chatbot/controller/SNABotController.java
@@ -1,7 +1,7 @@
 package com.example.sna.chatbot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.sna.chatbot.dto.ChatbotRequestDTO;
 import com.example.sna.dialogflow.DialogFlowIntents;
 
+@CrossOrigin
 @RestController
 public class SNABotController {
 	
