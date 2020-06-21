0762f83aa2634361e498761034f1752be52b099e
sairaj.munigala <sairaj.munigala@ggktech.com>
2020-06-21 14:59:21 +0530

    Updated response format

--- src/main/java/com/example/sna/chatbot/controller/SNABotController.java
+++ src/main/java/com/example/sna/chatbot/controller/SNABotController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sna.chatbot.dto.ChatbotRequestDTO;
+import com.example.sna.chatbot.dto.ChatbotResponseDTO;
 import com.example.sna.dialogflow.DialogFlowIntents;
 
 @CrossOrigin
@@ -17,7 +18,7 @@ public class SNABotController {
 	DialogFlowIntents dilogFlowIntents;
 
 	@PostMapping("/getChatBotResponse")
-	public String getChatbotResponse(@RequestBody ChatbotRequestDTO chatbotRequest){
+	public ChatbotResponseDTO getChatbotResponse(@RequestBody ChatbotRequestDTO chatbotRequest){
 		 return dilogFlowIntents.detectIntentTexts(chatbotRequest.getQuestion(), chatbotRequest.getSessionID());
 	}
 }
--- src/main/java/com/example/sna/dialogflow/DialogFlowIntents.java
+++ src/main/java/com/example/sna/dialogflow/DialogFlowIntents.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.example.sna.chatbot.dto.ChatbotResponseDTO;
 import com.google.cloud.dialogflow.v2.DetectIntentResponse;
 import com.google.cloud.dialogflow.v2.QueryInput;
 import com.google.cloud.dialogflow.v2.QueryResult;
@@ -14,39 +15,41 @@ import com.google.cloud.dialogflow.v2.TextInput.Builder;
 
 @Service
 public class DialogFlowIntents {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(DialogFlowIntents.class);
-	
+
 	private String projectId = "snachatbot-rdynhe";
 
-	public String detectIntentTexts(String text, String sessionId) {
-			String result = "";
-		    // Instantiates a client
-		    try (SessionsClient sessionsClient = SessionsClient.create()) {
-		      // Set the session name using the sessionId (UUID) and projectID (my-project-id)
-		      SessionName session = SessionName.of(projectId, sessionId);
+	public ChatbotResponseDTO detectIntentTexts(String text, String sessionId) {
+		ChatbotResponseDTO chatbotResponseDTO = new ChatbotResponseDTO();
+		String result = "";
+		// Instantiates a client
+		try (SessionsClient sessionsClient = SessionsClient.create()) {
+			// Set the session name using the sessionId (UUID) and projectID (my-project-id)
+			SessionName session = SessionName.of(projectId, sessionId);
 
-		      // Detect intents for each text input
-	        // Set the text (hello) and language code (en-US) for the query
-	        Builder textInput = TextInput.newBuilder().setText(text).setLanguageCode("en-US");
+			// Detect intents for each text input
+			// Set the text (hello) and language code (en-US) for the query
+			Builder textInput = TextInput.newBuilder().setText(text).setLanguageCode("en-US");
 
-	        // Build the query with the TextInput
-	        QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+			// Build the query with the TextInput
+			QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
 
-	        // Performs the detect intent request
-	        DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+			// Performs the detect intent request
+			DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
 
-	        // Display the query result
-	        QueryResult queryResult = response.getQueryResult();
+			// Display the query result
+			QueryResult queryResult = response.getQueryResult();
 
-	        logger.info("query text :" + queryResult.getQueryText());
-	        logger.info("Detected Intent: "+ queryResult.getIntent().getDisplayName() + "(confidence: )\n" +
-	             queryResult.getIntentDetectionConfidence());
-	        logger.info("Fulfillment Text: \n" + queryResult.getFulfillmentText());
-	        result = queryResult.getFulfillmentText();
-	      } catch (Exception e) {
+			logger.info("query text :" + queryResult.getQueryText());
+			logger.info("Detected Intent: " + queryResult.getIntent().getDisplayName() + "(confidence: )\n"
+					+ queryResult.getIntentDetectionConfidence());
+			logger.info("Fulfillment Text: \n" + queryResult.getFulfillmentText());
+			result = queryResult.getFulfillmentText();
+		} catch (Exception e) {
 			logger.error(e.getMessage());
-	      }
-		return result;    
-	  }
+		}
+		chatbotResponseDTO.setData(result);
+		return chatbotResponseDTO;
+	}
 }
