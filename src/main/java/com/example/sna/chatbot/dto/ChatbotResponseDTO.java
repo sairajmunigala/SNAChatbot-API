1ec1f9debda1aa42014d2929c65ffbf22473f8cd
sairaj.munigala <sairaj.munigala@ggktech.com>
2020-06-21 15:00:12 +0530

    Pushing missed files

---
+++ src/main/java/com/example/sna/chatbot/dto/ChatbotResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.sna.chatbot.dto;
+
+public class ChatbotResponseDTO {
+	
+	private String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+}
